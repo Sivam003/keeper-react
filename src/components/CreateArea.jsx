e15d848f5967abcd7a1e5433995c62622641abc7
Sivam <sivam.colg@gmail.com>
2024-07-14 21:22:33 +0530

    updated css and added icons

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,30 +1,54 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Zoom } from "@mui/material";
 
 function CreateArea(props) {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [active, setActive] = useState(false);
 
   function handleTitleChange(event) {
     setTitle(event.target.value);
-  };
+  }
 
   function handleContentChange(event) {
     setContent(event.target.value);
-  };
+  }
 
   function handleSubmit(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); 
     props.onSubmit(title, content);
-    setTitle(""); // Reset the title field
-    setContent(""); // Reset the content field
-  };
+    setTitle(""); 
+    setContent(""); 
+  }
+
+  function handleActive() {
+    setActive(true);
+  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleTitleChange} name="title" placeholder="Title" value={title}/>
-        <textarea onChange={handleContentChange} name="content" placeholder="Take a note..." rows="3" value={content}/>
-        <button>Add</button>
+      <form className="create-note" onSubmit={handleSubmit}>
+        <input
+          onChange={handleTitleChange}
+          name="title"
+          placeholder="Title"
+          value={title}
+          onClick={handleActive}
+        />
+        {active && <textarea
+          onChange={handleContentChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={content}
+        />}
+
+        <Zoom in={active}>
+          <button>
+            <AddIcon />
+          </button>
+        </Zoom>
       </form>
     </div>
   );
